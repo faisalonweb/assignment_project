319631197af9397c04e69cd26159e283b966f298
Faisal Usman <faisalusman390@gmail.com>
2023-09-15 19:59:54 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import SubHeader from "./components/SubHeader/SubHeader";
 import StepperDetail from "./components/stepper/Stepper";
 import Card from "./components/Card/Card";
